fd2831c67e5ddf7eabf37586e9c857822acc3ba9
AgSmith95 <ivan.a.kornienko@gmail.com>
2026-07-11 12:50:36 +0300

    mouse moving and F2 right click

--- windows/mouse_mover/mmover.cpp
+++ windows/mouse_mover/mmover.cpp
@@ -17,14 +17,16 @@ extern "C" {
 
 using namespace std::chrono_literals;
 
-const unsigned CYCLES_TO_WAIT = 480;
+constexpr std::chrono::milliseconds DELAY_BETWEEN_MOVES = 100ms;
+constexpr std::chrono::milliseconds DELAY_BETWEEN_CHECKS = 500ms;
+const unsigned CYCLES_TO_WAIT = 120;
 
 void reg_hotkey_wrapper(UINT vk, UINT mod = MOD_NOREPEAT) {
     if (RegisterHotKey(NULL, 1, mod, vk))
     {
-#ifdef _DEBUG
+		#ifdef _DEBUG
         std::cout << "Hotkey '0x" << std::hex << vk << "' registered, using MOD_NOREPEAT flag\n";
-#endif // _DEBUG
+		#endif // _DEBUG
     }
     else
     {
@@ -40,12 +42,13 @@ std::atomic<bool> random_moving = false;
 std::random_device rd;
 std::mt19937 gen(rd());
 std::uniform_int_distribution<> ud(-20, 20);
+std::atomic<POINT> global_pos{POINT{0,0}};
 void move_mouse() {
     unsigned counter = 0;
     POINT curr_pos{0,0};
     POINT pos{0,0};
     GetCursorPos(&curr_pos);
-    while (!exit_condition) {
+    while (!exit_condition.load()) {
         if (random_moving.load()) {
             // 1. SetCursorPos - doesn't work mouse is not really moving
             //GetCursorPos(&cursorPos);
@@ -64,18 +67,21 @@ void move_mouse() {
             //SendInput(1, &input, sizeof(input));
 
             // 3. mouse_event
-            mouse_event(MOUSEEVENTF_MOVE, ud(gen), ud(gen), 0, 0);
-            std::this_thread::sleep_for(20ms);
-
+			int dx = ud(gen);
+			int dy = ud(gen);
+            mouse_event(MOUSEEVENTF_MOVE, dx, dy, 0, 0);
+			GetCursorPos(&curr_pos);
+			global_pos.store(curr_pos);
+            std::this_thread::sleep_for(DELAY_BETWEEN_MOVES);
         }
         else {
-            std::this_thread::sleep_for(500ms);
+            std::this_thread::sleep_for(DELAY_BETWEEN_CHECKS);
             GetCursorPos(&pos);
             if (pos.x == curr_pos.x && pos.y == curr_pos.y) {
                 ++counter;
-#ifdef _DEBUG
+				#ifdef _DEBUG
                 std::cout << "tick " << counter << '\n';
-#endif
+				#endif
                 if (counter >= CYCLES_TO_WAIT) {
                     counter = 0;
                     random_moving.store(true);
@@ -105,13 +111,37 @@ void move_mouse() {
 //    }
 //}
 
+void toggleMoving(bool &movingEnabled)
+{
+	#ifdef _DEBUG
+	std::cout << "MOVING ";
+	if (!random_moving) {
+		std::cout << "START";
+	}
+	else {
+		std::cout << "STOP";
+	}
+	std::cout << '\n';
+	#endif // _DEBUG
+	movingEnabled = random_moving.load();
+	while (!random_moving.compare_exchange_weak(movingEnabled, !movingEnabled)) {}
+}
+
+void rightClick()
+{
+	POINT cursorPos{};
+	GetCursorPos(&cursorPos);
+	mouse_event(MOUSEEVENTF_RIGHTDOWN, cursorPos.x, cursorPos.y, 0, 0);
+	mouse_event(MOUSEEVENTF_RIGHTUP, cursorPos.x, cursorPos.y, 0, 0);
+}
+
 int main()
 {
     //reg_hotkey_wrapper(VK_ESCAPE);
     reg_hotkey_wrapper(VK_F1);
-    //reg_hotkey_wrapper(VK_F2);
+    reg_hotkey_wrapper(VK_F2);
 
-    std::thread mover(move_mouse);
+    //std::thread mover(move_mouse);
     //std::thread clicker(left_click);
 
     std::cout <<
@@ -123,46 +153,37 @@ int main()
               "===== ===== =====\n";
 
     MSG msg = { 0 };
-    bool old;
+    bool movingEnabled = false;
     while (GetMessage(&msg, NULL, 0, 0) != 0)
     {
         //std::cout << "\nMessage:" << msg.message;
         if (msg.message == WM_HOTKEY)
         {
-#ifdef _DEBUG
+			#ifdef _DEBUG
             std::cout << "-> WM_HOTKEY 0x" << std::hex << msg.message << " received\n"
                       << "    lParam = " << std::hex << htonl(msg.lParam)
                       <<   "; wParam = " << std::hex << htonl(msg.wParam)
                       << '\n';
-#endif // _DEBUG
+			#endif // _DEBUG
 
             switch (htonl(msg.lParam) >> 8)
             {
                 case VK_ESCAPE:
                     std::cout << "Exiting\n";
                     exit_condition = true;
-                    mover.join();
+                    //mover.join();
                     //clicker.join();
                     return 0;
                 case VK_F1:
-#ifdef _DEBUG
-                    std::cout << "MOVING ";
-                    if (!random_moving) {
-                        std::cout << "START";
-                    }
-                    else {
-                        std::cout << "STOP";
-                    }
-                    std::cout << '\n';
-#endif // _DEBUG
-                    old = random_moving.load();
-                    while (!random_moving.compare_exchange_weak(old, !old)) {}
-                    break;
-//            case VK_F2:
-//#ifdef _DEBUG
-//                std::cout << "CLICKING\n";
-//#endif // _DEBUG
-//                clicking_flag = clicking_flag ? false : true;
+				{
+					toggleMoving(movingEnabled);
+					break;
+				}
+				case VK_F2:
+				{
+					rightClick();
+					break;
+				}
                 default:
                     break;
             }
