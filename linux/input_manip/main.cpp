9124a693fa4f68d2bd1e43ae4655d3d386e348b4
AgSmith95 <ivan.a.kornienko@gmail.com>
2026-07-12 19:36:10 +0300

    linux input_manip is_keyboard_with_F2 v2
    left block commented out intentionally

--- linux/input_manip/main.cpp
+++ linux/input_manip/main.cpp
@@ -73,17 +73,17 @@ void keyboard_listener(int fd, std::string device_path) {
 
 // checks if a device is a keyboard that supports F2 key
 bool is_keyboard_with_F2(int fd) {
-	unsigned long evbit[NBITS(EV_MAX)];
-	memset(evbit, 0, sizeof(evbit));
-	// Get supported event types (EV_SYN, EV_KEY, EV_REL, etc.)
-	if (ioctl(fd, EVIOCGBIT(0, sizeof(evbit)), evbit) < 0) {
-		DLOG(std::cerr << "ERROR: ioctl EVIOCGBIT(0)\n";)
-		return false;
-	}
-	// Check if EV_KEY type is supported, if not - not a keyboard
-	if (!TEST_BIT(EV_KEY, evbit)) {
-		return false;
-	}
+	// unsigned long evbit[NBITS(EV_MAX)];
+	// memset(evbit, 0, sizeof(evbit));
+	// // Get supported event types (EV_SYN, EV_KEY, EV_REL, etc.)
+	// if (ioctl(fd, EVIOCGBIT(0, sizeof(evbit)), evbit) < 0) {
+	// 	DLOG(std::cerr << "ERROR: ioctl EVIOCGBIT(0)\n";)
+	// 	return false;
+	// }
+	// // Check if EV_KEY type is supported, if not - not a keyboard
+	// if (!TEST_BIT(EV_KEY, evbit)) {
+	// 	return false;
+	// }
 	// Check if F2 button is on the keyboard
 	unsigned long keybit[NBITS(KEY_MAX)];
 	memset(keybit, 0, sizeof(keybit));
